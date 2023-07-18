b1f679b3d07343a7c0465e2b26ede84ddd57741d
bughunter98 <saitejanaiduthota@gmail.com>
2023-07-18 21:20:47 +0530

    Adding production ready features and swagger functions

--- src/main/java/com/udemypractise/SpringBootTutorialPractise/SpringBootTutorialPractiseApplication.java
+++ src/main/java/com/udemypractise/SpringBootTutorialPractise/SpringBootTutorialPractiseApplication.java
@@ -1,11 +1,20 @@
 package com.udemypractise.SpringBootTutorialPractise;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.udemypractise.SpringBootTutorialPractise"})
+@OpenAPIDefinition(info = @Info(
+		title = "Spring Boot REST API Documentation",
+		description = "Spring Boot REST API Documentation",
+		version = "v1.0",
+		contact = @Contact(name = "Sai Teja" , email = "venkatasaitejathota97@gmail.com")
+))
 public class SpringBootTutorialPractiseApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/udemypractise/SpringBootTutorialPractise/controller/StudentController.java
+++ src/main/java/com/udemypractise/SpringBootTutorialPractise/controller/StudentController.java
@@ -3,6 +3,9 @@ package com.udemypractise.SpringBootTutorialPractise.controller;
 import com.udemypractise.SpringBootTutorialPractise.DTO.StudentDto;
 import com.udemypractise.SpringBootTutorialPractise.model.StudentEntity;
 import com.udemypractise.SpringBootTutorialPractise.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.RequestEntity;
@@ -11,6 +14,8 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Tag(name = "CRUD REST APIs for User Resource",
+description = "CRUD REST APIs -Create User , Update User , Get User , Get All Users , Delete User")
 @RestController
 @RequestMapping("api/student")
 public class StudentController {
@@ -18,26 +23,46 @@ public class StudentController {
     @Autowired
     private UserService userService;
 
+    @Operation(summary = "Create Student REST API",
+    description = "This API is used to create/add the Student details to DB")
+    @ApiResponse(responseCode = "201",
+    description = "HTTP Status 201 Created")
     @PostMapping("createStudent")
     public StudentDto createStudent(@RequestBody StudentDto student) throws Exception {
         return userService.createUser(student);
     }
+    @Operation(summary = "Create Student REST API with Response Entity Added",
+            description = "This API is used to create/add the Student details to DB")
+    @ApiResponse(responseCode = "201",
+            description = "HTTP Status 201 Created")
     @PostMapping("createStudent1")
     public ResponseEntity<StudentDto> createStudentwithResponseEntity(@RequestBody StudentDto student) throws Exception {
         StudentDto user = userService.createUser(student);
         return new ResponseEntity<>(user,HttpStatus.CREATED);
     }
     @PostMapping("createStudent2")
+    @Operation(summary = "Create Student REST API with Response Entity and Headers Added",
+            description = "This API is used to create/add the Student details to DB")
+    @ApiResponse(responseCode = "201",
+            description = "HTTP Status 201 Created")
     public ResponseEntity<StudentEntity> createStudentwithRequestandResponseEntity(RequestEntity<StudentEntity> student) throws Exception {
         StudentEntity user = userService.createUserwithRequestEntity(student);
         return new ResponseEntity<>(user,HttpStatus.CREATED);
     }
 
     @GetMapping("getStudent/{id}")
+    @Operation(summary = "Get Student REST API",
+            description = "This API is used to get the Student details from DB")
+    @ApiResponse(responseCode = "400",
+            description = "HTTP Status 400 Found")
     public ResponseEntity<StudentDto> getStudentbyId(@PathVariable("id") int studentId) throws Exception {
         StudentDto studentDetails = userService.getStudentbyId(studentId);
         return new ResponseEntity<>(studentDetails,HttpStatus.FOUND);
     }
+    @Operation(summary = "Get Student REST API",
+            description = "This API is used to get all the Student details from DB")
+    @ApiResponse(responseCode = "400",
+            description = "HTTP Status 400 Found")
     @GetMapping("getAllStudents")
     public ResponseEntity<List<StudentDto>> getAllStudents() throws Exception {
         List<StudentDto>studentDtos = userService.getAllStudents();
@@ -48,12 +73,20 @@ public class StudentController {
     //Put mapping can be used if and only if all the fields in the pojo are available in request
     //generally for put mapping we need to add all the fields of pojo in json.
     //here just for example , i tried to implement put mapping with any one field. so this would act as patch mapping as well.
+    @Operation(summary = "Update Student REST API",
+            description = "This API is used to modify the Student details from DB")
+    @ApiResponse(responseCode = "200",
+            description = "HTTP Status 200 Created")
     public ResponseEntity<StudentDto> updateStudent(@RequestBody StudentDto student) throws Exception {
         StudentDto studentDto = userService.updateStudents(student);
         return new ResponseEntity<>(studentDto,HttpStatus.OK);
     }
 
     @DeleteMapping("deleteStudent/{id}")
+    @Operation(summary = "Delete Student REST API",
+            description = "This API is used to Delete the Student details from DB")
+    @ApiResponse(responseCode = "200",
+            description = "HTTP Status 200 Found")
     public ResponseEntity<String> deleteStudent(@PathVariable("id") int studentid) throws Exception{
         userService.deleteStudentbyId(studentid);
         return new ResponseEntity<>("Student Data Deleted",HttpStatus.OK);
